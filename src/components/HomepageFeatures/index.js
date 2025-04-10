30baaebadb22d870bb77341b1848bdd714cb234a
Erik Andersson <erik.andersson@signupsoftware.com>
2025-04-10 11:24:18 +0200

    Quick fix for EDC link from Main meny

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -69,7 +69,8 @@ const FeatureList = [
   },
   {
     title: <Translate>EXFLOW DATA CAPTURE</Translate>,
-    url: 'https://docs.signupsoftware.com/user-manual/under-dev',
+    //url: 'https://docs.signupsoftware.com/user-manual/under-dev',
+    url: "https://docs.signupsoftware.com/datacapture/docs/user-manual/under-dev",
     index: 4,
     tag: 'data-capture'
   },
@@ -177,3 +178,4 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
